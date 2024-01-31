966b52ead4ef3fe3b8cad57876bc73f58575c50b
Vikram <vikramkrraj9876@gmail.com>
2024-01-31 18:24:59 +0530

    third commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMWa5Hd8ntNm6YgNeDZv_BCI1IpHMcKxU",
+  authDomain: "linkedin-clone-74571.firebaseapp.com",
+  projectId: "linkedin-clone-74571",
+  storageBucket: "linkedin-clone-74571.appspot.com",
+  messagingSenderId: "6451817041",
+  appId: "1:6451817041:web:c952d5ccbb2d624d2ce670",
+  measurementId: "G-KC1NELL36X",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
+export { auth, app, firestore, storage };
